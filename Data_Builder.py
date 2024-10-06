565b6f55359c3b1b0a7fb9f13976470cb7b3b689
Messi Tom <messi10tombinoy@gmail.com>
2024-10-06 10:47:56 +0530

    parser initialised

---
+++ Data_Builder.py
@@ -0,0 +1,10 @@
+from PDFParser import PDFparser
+from pathlib import Path
+
+PDFDIR = Path('Data') / 'pdfs'
+parser = PDFparser()
+
+for pdf in PDFDIR.glob('*.pdf'):
+    parser.parse(pdf)
+
+parser.sample()
---
+++ PDFParser.py
@@ -0,0 +1,45 @@
+from pypdf import PdfReader
+
+class PDFparser:
+    """
+    returns a collection of text from a PDF file
+    
+    """
+
+    def __init__(self, filename=None):
+        if filename:
+             self.filename = filename if type(filename) == list  else [filename]
+        else: 
+            self.filename = []
+        self.pages = None
+        self.text = None
+
+    def parse(self, filename=None):
+        if self.filename:
+            reader = PdfReader(self.filename)
+
+        if filename:
+            self.filename.append(filename)
+            reader = PdfReader(self.filename)
+            self.pages = len(reader.pages)
+            self.text = {filename: {ix: page.extract_text for ix, page in enumerate(reader.pages)}}
+        
+
+    def sample(self, page=1, lines=10):
+        print(self.filename[0])
+        print(self.text[self.filename[0]][page][:lines])
+
+    def get_num_of_pages(self):
+        return len(self.text.split())
+
+    def get_num_of_chars(self):
+        return len(self.text)
+
+    def get_num_of_lines(self):
+        return self.text.count('\n')
+
+    def get_num_of_paragraphs(self):
+        return self.text.count('\n\n')
+
+    def get_num_of_sentences(self):
+        return self.text.count('.') + self.text.count('?') + self.text.count('!')
