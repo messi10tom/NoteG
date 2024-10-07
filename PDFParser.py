a60656ee1b5dc3543ea89d896ae8ce1151f0d072
Messi Tom <messi10tombinoy@gmail.com>
2024-10-07 09:56:49 +0530

    Langchain integration

--- PDFParser.py
+++ PDFParser.py
@@ -1,46 +1,30 @@
-from PyPDF2 import PdfReader
+from langchain_community.document_loaders import PyPDFLoader
+from pathlib import Path
 
-class PDFparser:
-    """
-    returns a collection of text from a PDF file
+PDFDIR = Path('Data') / 'pdfs'
+
+
+def pdfparser(dir: Path) -> dict:
+
+    parser = {}
+
+    for pdf in dir.glob('*.pdf'):
+        pdf_loader = PyPDFLoader(
+            file_path = pdf,
+            extract_images = True
+        )
+        parser[pdf.name] = pdf_loader
+
+    return parser
+
+parser = pdfparser(PDFDIR)
+sample_loader = next(iter(parser.values()))
+
+docs = []
+docs_lazy = sample_loader.lazy_load()
+
+for doc in docs_lazy:
+    docs.append(doc)
     
-    """
-
-    def __init__(self, filename=None):
-        if filename:
-             self.filename = filename if type(filename) == list  else [filename]
-        else: 
-            self.filename = []
-        self.pages = None
-        self.text = {}
-
-    def parse(self, filename=None):
-        if self.filename:
-            reader = PdfReader(self.filename)
-
-        if filename:
-            self.filename.append(filename)
-            
-            reader = PdfReader(filename)
-            self.pages = len(reader.pages)
-            self.text[filename] = {ix: page.extract_text() for ix, page in enumerate(reader.pages)}
-        
-
-    def sample(self, page=1, lines=100):
-        print(self.filename[0])
-        print(self.text[self.filename[0]][page][:lines])
-
-    def get_num_of_pages(self):
-        return len(self.text.split())
-
-    def get_num_of_chars(self):
-        return len(self.text)
-
-    def get_num_of_lines(self):
-        return self.text.count('\n')
-
-    def get_num_of_paragraphs(self):
-        return self.text.count('\n\n')
-
-    def get_num_of_sentences(self):
-        return self.text.count('.') + self.text.count('?') + self.text.count('!')
+print(docs[0].page_content[:100])
+print(docs[0].metadata)
